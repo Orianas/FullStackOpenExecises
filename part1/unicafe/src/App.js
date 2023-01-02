89c62b13f22e03ecaf9078a48bc69e483740f7a8
Steve Gilland <steven.gilland@gmail.com>
2023-01-02 00:59:54 -0600

    Exercise 1.6 Completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 
+const Header = () => <h1>Give Feedback</h1>
+const StatDisplay = ({ label, value }) => <div>{label}: {value}</div>
+const Statistics = ({ good, neutral, bad }) => {
+  console.log(good, neutral, bad)
+  return (
+    <>
+      <h1>Statistics</h1>
+      <StatDisplay label={'good'} value={good} />
+      <StatDisplay label={'neutral'} value={neutral} />
+      <StatDisplay label={'bad'} value={bad} />
+    </>
+  )
+}
+
+const Button = ({ handleClick, text }) =>
+  <button onClick={handleClick}>{text}</button>
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <Header />
+      <Button handleClick={increaseGood} text='good' />
+      <Button handleClick={increaseNeutral} text='neutral' />
+      <Button handleClick={increaseBad} text='bad' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
